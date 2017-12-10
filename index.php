1430041d047e6b38db70ff8cd0bd4f7f72e57215
kmilcarz <j.milcarz@yahoo.com>
2017-12-10 21:34:34 +0100

    problems with timeline :/

--- index.php
+++ index.php
@@ -12,6 +12,23 @@
 <body>
      <?php if (Auth::loggedin()) { ?>
           <form action="" method="post"><input type="submit" name="logoutbtn" value="wyloguj się"></form>
+
+          <?php
+               # timeline
+               if (!DB::query('SELECT friends_friendid FROM friends, users WHERE user_user_id=friends_userid OR user_user_id=friends_friendid AND friends_status=1')) {
+                    $posts = DB::query('SELECT * FROM posts, users WHERE posts_userid=user_user_id AND user_user_id=:userid ORDER BY posts_id DESC', [':userid'=>Auth::loggedin()]);
+                    foreach ($posts as $post) {
+                         echo '<a href="post/' . $post['posts_id'] .'">' . $post['user_full_name'] . " ~ " . $post['posts_body'] . " (" . $post['posts_date'] . ") " . "<br><hr>" . '</a>';
+                    }
+               }else {
+                    $posts = DB::query('SELECT * FROM posts, users, friends WHERE user_user_id=posts_userid AND posts_privacy=2 AND friends_status=4 AND (user_user_id=friends_userid OR user_user_id=friends_friendid) ORDER BY posts_id DESC');
+                    foreach ($posts as $post) {
+                         echo '<a href="post/' . $post['posts_id'] .'">' . $post['user_full_name'] . " ~ " . $post['posts_body'] . " (" . $post['posts_date'] . ") " . "<br><hr>" . '</a>';
+                    }
+               }
+
+          ?>
+
      <?php } ?>
      <script src="assets/js/juery.js"></script>
      <script src="assets/js/functions.js"></script>
